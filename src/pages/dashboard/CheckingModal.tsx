8647fefa491026ee6981dfd8f8db235d0e5fde00
eric.khanh <khanhdangbmt@gmail.com>
2025-12-07 18:18:42 +0700

    fix: remove copy icon on the checking modal

--- src/pages/dashboard/CheckingModal.tsx
+++ src/pages/dashboard/CheckingModal.tsx
@@ -1,15 +1,12 @@
-import { ContentCopy } from '@/components/icons';
 import { Modal } from '@/components/modal/Modal';
 import { Table, TableColumn } from '@/components/table/Table';
 import { useResponsive } from '@/hooks/useResponsive';
 import { serverService } from '@/services/server/server.service';
 import { NodeStatus } from '@/services/server/server.types';
 import { itemVariants } from '@/utils/animation';
-import { copyToClipboard } from '@/utils/copyToClipboard';
 import { useQuery } from '@tanstack/react-query';
 import { motion } from 'framer-motion';
 import { useMemo } from 'react';
-import { toast } from 'sonner';
 
 export const CheckingModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
   const { data, isLoading } = useQuery({
@@ -31,20 +28,7 @@ export const CheckingModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: (
       {
         key: 'public_ip',
         title: 'IP',
-        align: 'center',
-        render: (_value, record) => (
-          <div className="flex items-center justify-center">
-            <span>{record.public_ip}</span>
-            <ContentCopy
-              className="text-blue cursor-pointer ml-2"
-              onClick={(e) => {
-                e.stopPropagation();
-                copyToClipboard(record.public_ip);
-                toast.success('Đã sao chép IP vào clipboard');
-              }}
-            />
-          </div>
-        )
+        align: 'center'
       },
       {
         key: 'score',

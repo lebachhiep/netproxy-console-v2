251129779bbdc664d41878974bf5fe481a23753e
Nari <dative_filters.0c@icloud.com>
2025-12-15 13:07:16 +0700

    fix(order): update expired column key

--- src/pages/order/OrderDetailPage.tsx
+++ src/pages/order/OrderDetailPage.tsx
@@ -583,7 +583,7 @@ const OrderDetailPage = () => {
       },
       {
         width: 150,
-        key: 'next_renewal_date',
+        key: 'expires_at',
         title: t('expired'),
         render: (value) => <div className="font-semibold">{moment(value).format('DD/MM/YYYY HH:mm')}</div>
       },
